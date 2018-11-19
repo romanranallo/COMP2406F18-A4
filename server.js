bd6d9ad2262e7459782b700eeb2af3bd2921c2c2
Richard St. John <rgeoffstjohn1@gmail.com>
2018-11-19 18:40:36 -0500

    basically working even though it's not pretty

--- server.js
+++ server.js
@@ -5,25 +5,92 @@
 */
 
 const express = require("express")
+const bodyParser = require("body-parser")
+const logger = require("morgan")
+const expRequest = require("request")
+
 const app = express()
+
 
 const PORT = process.env.PORT || 3000
 const ROOT_DIR = "/public"
 const path = require("path")
+const API_KEY = "3838769ed4fcbfcf91eb9306280948db"
+const HOST = "https://www.food2fork.com/api/search"
 
-function serveResponse(request, response) {
+function sendResponse(body, response) {
+	body = JSON.parse(body)
 	
-	//Add code to get Json and then inject html div elements with class="photos" into id="photoArea
+	let page = `<html lang = "en-US"><head><meta charset = "UTF-8">
+		<link rel="stylesheet" type="text/css" href="/public/styles/style.css">
+		<title>Recipes</title> 
+		</head><body>
+		<form id="queryForm" method="post">
+		Ingredients: <input name="ingredients">
+		<input type="submit" value="Get Recipes">
+		</form><div id="photoArea">`
+	if (body !== null){
+		for(let i=0; i<body.count; i++) {
+			page += `<div class="photo">
+					<a href="${body.recipes[i].f2f_url}" target="_blank">
+					<img class="photo" src="${body.recipes[i].image_url}"></a>
+					<p class="photo">${body.recipes[i].title}</div>`
+		}
+	}
+	page +=	`<p></p></div></body></html>`
 	
-	response.sendFile(path.join(__dirname + ROOT_DIR +"/index.html"))
+	response.send(page)
 }
 
-app.get("/recipes.html", serveResponse)
-app.get("/recipes")
-app.get("/index.html", serveResponse)
-app.get("/", serveResponse)
-app.get("", serveResponse)
+function sendGetRequest(ingredients, res) {
+	let url = HOST + `?q=${ingredients}&key=${API_KEY}`
+	expRequest.get(url, function(error, response, body) {
+		if(error) {
+			console.log(`Error:${error}`)
+			sendResponse(null, res)
+		}
+		console.log(body)
+		sendResponse(body, res)
+	})
+}
 
+function postResponse(request, response) {
+	let ingredients = request.body.ingredients
+	console.log(ingredients)
+	sendGetRequest(ingredients, response)
+}
+
+function getResponse(request, response) {
+	//Add code to get Json and then inject html div elements with class="photos" into id="photoArea
+	let ingredients = request.query.ingredients
+	console.log(ingredients)
+	if(ingredients) {
+		sendGetRequest(ingredients, response)
+	}
+	else {
+		sendResponse(null, response)
+	}
+	//response.sendFile(path.join(__dirname + ROOT_DIR +"/index.html"))
+}
+
+app.use(logger("dev"))
+app.use(bodyParser.urlencoded({}))
+app.use(bodyParser.json())
+
+app.get("/recipes.html", getResponse)
+app.get("/recipes", getResponse)
+app.get("/index.html", getResponse)
+app.get("/", getResponse)
+app.get("", getResponse)
+app.get("/public/styles/style.css", function(req, res) {
+	res.sendFile(__dirname + ROOT_DIR + "/styles/style.css")
+	})
+app.post("/recipes", postResponse)
+app.post("/recipes.html", postResponse)
+app.post("/recipes", postResponse)
+app.post("/index.html", postResponse)
+app.post("/", postResponse)
+app.post("", postResponse)
 
 app.listen(PORT, err => {
   if (err) console.log(err)
